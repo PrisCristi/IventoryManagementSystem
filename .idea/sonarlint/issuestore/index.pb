00aea78a77c03be3cb8e9fb0af7fbeefa8bcf8e6
PrisCristi <santos.priscila85@gmail.com>
2023-10-15 13:57:44 +0200

    Class item was created

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+S
+#src/main/java/org/example/Main.java,0/b/0ba1b9c7a4878802f938e331cc2d8a042087ec6b
+S
+#src/main/java/org/example/Item.java,c/3/c308bc052f4c4e9843ead211398ba2d4eb0b1f96
---
+++ src/main/java/org/example/Item.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Item {
+    private int id;
+    private String name;
+    private int quantity;
+
+    public Item (int id, String name, int quantity){   // constructor
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+
+    }
+}
