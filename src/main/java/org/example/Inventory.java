9e015c0fcf1bc44a0e4d0e2b7b619a8a05f34dd5
PrisCristi <santos.priscila85@gmail.com>
2023-10-15 19:50:23 +0200

    Updated Inventory class to became Generics

--- src/main/java/org/example/Inventory.java
+++ src/main/java/org/example/Inventory.java
@@ -3,20 +3,19 @@ package org.example;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Inventory {
-    private List<Item> items;                   // private List <int> numero;
+public class Inventory <T>{
+    private List<T> items;                   // private List <int> numero;
 
     public Inventory(){  // constructor             public Inventory {
         items = new ArrayList<>(); //           // numeros = new ArrayList<>();
     }
-    public void addItem(Item item){            // public void addNumeros (int numero).
+    public void addItem(T item){            // public void addNumeros (int numero).
         items.add(item);                       // numeros.add(numero)
     }
 
-    public void removeItem(Item item) throws ItemNotFoundException{
-        if(!items.contains(item)){
-            throw new ItemNotFoundException( "Item not Found! ");
-        }
+    public void removeItem(T item) throws ItemNotFoundException{
+        if(!items.contains(item))
+            throw new ItemNotFoundException("Item not Found! ");
         items.remove(item);  // se o item estiver na lista será removido por esse metodo.
     }
 }
