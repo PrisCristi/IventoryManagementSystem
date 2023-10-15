092ee5f267fbac9f3e9743ae4f7e158ed9eb515e
PrisCristi <santos.priscila85@gmail.com>
2023-10-15 21:00:58 +0200

    Created main class and tested - it is working.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,22 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       Inventory<FoodItem> foodInventory = new Inventory<>();
+
+       FoodItem foodItem = new FoodItem(01,"Banana", 45);
+       try {
+           foodInventory.removeItem(foodItem);
+       } catch (ItemNotFoundException e){
+
+           System.out.println("Sorry, "+ e.getMessage());
+       }
+      try {
+          foodInventory.addItem(foodItem);
+
+          foodInventory.removeItem(foodItem);
+      } catch (ItemNotFoundException e) {
+          throw new RuntimeException(e);
+      }
+
     }
 }
